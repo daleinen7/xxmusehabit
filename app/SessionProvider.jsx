dba0514752b460f8b916a804ef3132f25c8df6e3
Doug Leinen <doug@geletkaplus.com>
2023-11-19 17:19:54 -0600

    auth-zero setup

--- app/SessionProvider.jsx
+++ app/SessionProvider.jsx
@@ -1,6 +1,5 @@
-'use client';
-import { Session } from 'next-auth';
-import { SessionProvider as Provider } from 'next-auth/react';
+"use client";
+import { SessionProvider as Provider } from "next-auth/react";
 
 export default function SessionProvider({ children, session }) {
   return <Provider>{children}</Provider>;
