d7d1972e9325e83fd4385bf18f21b1e2bb9272c5
Doug Leinen <daleinen@gmail.com>
2024-01-11 20:51:59 -0600

    triple whatever

--- app/reset-password/page.jsx
+++ app/reset-password/page.jsx
@@ -3,7 +3,6 @@ import React, { useState, useEffect } from 'react';
 import Link from 'next/link';
 import { useRouter, useSearchParams } from 'next/navigation';
 import { confirmPasswordReset } from 'firebase/auth';
-import { auth } from '../../lib/firebase';
 import FormInput from '../components/FormInput';
 
 const formData = [
@@ -27,6 +26,7 @@ const ResetPassword = () => {
   const [form, setForm] = useState({
     email: '',
   });
+  const [success, setSuccess] = useState(false);
 
   const router = useRouter();
   const searchParams = useSearchParams();
@@ -42,9 +42,10 @@ const ResetPassword = () => {
     }
 
     try {
-      await confirmPasswordReset(auth, oobCode, newPassword);
+      await confirmPasswordReset(oobCode, newPassword);
       // Password reset successful, you can now redirect the user
-      router.push('/login');
+      // router.push('/login');
+      setSuccess(true);
     } catch (error) {
       console.error('Error resetting password:', error);
       alert('Password reset failed. Please try again.');
@@ -66,32 +67,40 @@ const ResetPassword = () => {
       </div>
       <div className="w-1/2 flex items-center justify-center">
         <div className="max-w-47rem flex flex-col justify-center items-center w-full max-w-[20.8125rem]">
-          <h2 className="font-satoshi text-[2.25rem] font-bold mb-[1.125rem">
-            Reset Password
-          </h2>
-          <form
-            onSubmit={handleSubmit}
-            className="flex flex-col justify-center gap-6 w-full"
-          >
-            {formData.map((item) => (
-              <FormInput
-                key={item.id}
-                label={item.label}
-                type={item.type}
-                id={item.id}
-                value={form[item.id]}
-                handleFormChange={handleFormChange}
-                required={item.required}
-                className="text-black p-[0.625rem] border border-black rounded-md w-full"
-              />
-            ))}
-            <button
-              type="submit"
-              className="mt-6 bg-gray-400 rounded-md px-[0.875] py-[0.625rem]"
-            >
-              Verify Email
-            </button>
-          </form>
+          {success ? (
+            <h2 className="font-satoshi text-[2.25rem] font-bold mb-[1.125rem">
+              Reset Successful!
+            </h2>
+          ) : (
+            <div>
+              <h2 className="font-satoshi text-[2.25rem] font-bold mb-[1.125rem">
+                Reset Password
+              </h2>
+              <form
+                onSubmit={handleSubmit}
+                className="flex flex-col justify-center gap-6 w-full"
+              >
+                {formData.map((item) => (
+                  <FormInput
+                    key={item.id}
+                    label={item.label}
+                    type={item.type}
+                    id={item.id}
+                    value={form[item.id]}
+                    handleFormChange={handleFormChange}
+                    required={item.required}
+                    className="text-black p-[0.625rem] border border-black rounded-md w-full"
+                  />
+                ))}
+                <button
+                  type="submit"
+                  className="mt-6 bg-gray-400 rounded-md px-[0.875] py-[0.625rem]"
+                >
+                  Verify Email
+                </button>
+              </form>
+            </div>
+          )}
         </div>
       </div>
     </div>
