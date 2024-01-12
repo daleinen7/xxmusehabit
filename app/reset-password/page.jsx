6f2b90504da2dfb42a4a9be94e9f473c9b088028
Doug Leinen <daleinen@gmail.com>
2024-01-11 20:36:48 -0600

    double whatever

--- app/reset-password/page.jsx
+++ app/reset-password/page.jsx
@@ -3,7 +3,7 @@ import React, { useState, useEffect } from 'react';
 import Link from 'next/link';
 import { useRouter, useSearchParams } from 'next/navigation';
 import { confirmPasswordReset } from 'firebase/auth';
-import { auth } from '../lib/firebase';
+import { auth } from '../../lib/firebase';
 import FormInput from '../components/FormInput';
 
 const formData = [
